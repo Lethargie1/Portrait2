8f47fb9dc1e9217e405d4ca5a4414be16cb955b0
Fabrice Valade <fabrice.valade@neoculus.com>
2020-04-20 11:23:42 -0400

    draft of modwritable

---
+++ SSEditor/FileHandling/Mods/SSModWritable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSEditor.FileHandling
+{
+    public class SSModWritable
+    {
+        public SSBaseLinkUrl ModUrl { get; private set; }
+        public SSJson ModInfo { get; private set; }
+        public string ModName { get; private set; }
+
+
+        public ObservableCollection<ISSWritable> FileList { get; set; } = new ObservableCollection<ISSWritable>();
+        
+        public SSModWritable()
+        { }
+
+
+        public override string ToString()
+        {
+            return "Mod: " + (ModInfo?.ReadToken("name")?.ToString() ?? ("Unnamed " + ModUrl.Link));
+        }
+    }
+}
