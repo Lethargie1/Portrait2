478c1e428cd431bb35c0c97a83bfd1c73c459e7d
Fabrice Valade <fabrice.valade@neoculus.com>
2020-05-12 16:54:25 -0400

    made viewmodels for writable

--- EditorInterface/ViewModel/FileVm/ISSWritableViewModel.cs
+++ EditorInterface/ViewModel/FileVm/ISSWritableViewModel.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using SSEditor.FileHandling;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
-namespace EditorInterface.ViewModel.FileVm
+namespace EditorInterface.ViewModel
 {
-    class ISSWritableViewModel
+    public class ISSWritableViewModel
     {
+        public ISSWritable Writable { get; set; }
+        public ISSWritableViewModel(ISSWritable writable)
+        {
+            Writable = writable;
+        }
+
+        public string filepath { get => Writable?.RelativeUrl.ToString(); }
+
+        public bool MustOverwrite { get => Writable?.MustOverwrite ?? false; }
+
+
+    }
+
+    [ValueConversion(typeof(SSFactionGroup), typeof(FactionGroupTokenViewModel))]
+    public class ISSWritableToISSWritableVMConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case SSJsonGroup j:
+                    return new JsonGroupViewModel(j);
+                case ISSWritable w:
+                    return new ISSWritableViewModel(w);
+                default:
+                    throw new ArgumentException("Converting a not writable");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- EditorInterface/ViewModel/FileVm/JsonGroupViewModel.cs
+++ EditorInterface/ViewModel/FileVm/JsonGroupViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using SSEditor.FileHandling;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
-namespace EditorInterface.ViewModel.FileVm
+namespace EditorInterface.ViewModel
 {
-    class JsonGroupViewModel
+    public class JsonGroupViewModel
     {
+        public SSJsonGroup JsonGroup { get; set; }
+        public JsonGroupViewModel(SSJsonGroup jsonGroup)
+        {
+            JsonGroup = jsonGroup;
+        }
+
+        public string filepath { get => JsonGroup?.RelativeUrl.ToString(); }
+
+        public bool MustOverwrite { get => JsonGroup?.MustOverwrite ?? false; }
+
     }
+
 }
